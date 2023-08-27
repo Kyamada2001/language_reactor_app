59966666027498f8ef74f8982a6d7e4fcd81b746
Kyamada2001 <yamakai173git@gmail.com>
2023-08-27 13:54:27 +0900

    みんなの翻訳TraのWebAPを使用して翻訳できるように

--- App.tsx
+++ App.tsx
@@ -27,23 +27,51 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
+import oauth from 'axios-oauth-client'
 
 const youtubeHeight = Dimensions.get('window').width / 16 * 9;
+const translateJsonData = require('./assets/language/en_to_ja.json')
 
 function App(): JSX.Element {
   const playerRef: any = useRef();
   const [subtitles, setSubtitles] = useState([]);
   const [videoId, setVideoId] = useState("lhr4Ax4C_-4");
+  // TODO: URL系は環境変数で管理する。
+  const [googleTranslateUrl, setGoogleTranslateUrl] = useState("https://script.google.com/macros/s/AKfycbyNX-jodYhhZESYMQ9hzDxtzRgs_y4nWgGGFGNB1A8rR_Y9Kn1w1djVZtv1pGKAvA4f/exec")
 
-  const [captionIndex, setCaptionIndex] = useState(Number);
-  const [currCaptions, setCurrCaptions] = useState(Array<Object>)
+  const [captionIndex, setCaptionIndex] = useState<number | null>(null); // 字幕全体に対する配列のIndex
+  const [currCaptions, setCurrCaptions] = useState(Array<Object>) // 現在再生している字幕情報
   // const [charCaption, setCharCaption] = useState(Array<Array<String>>) この機能の実装はまだ先
-  const [currentTime, setCurrentTime] = useState(Number);
+  const [currentTime, setCurrentTime] = useState<number | null>(null);
   const [pressedVideo, setPressedVideo] = useState(Boolean)
-  const [translateWord, setTranslateWord] = useState(null)
-  const [translateWordId, setTranslateWordId] = useState(null)
+  const [sourceVideoCaption, setSourceVideoCaption] = useState<string | null>("environment") // ビデオ字幕の翻訳対象文字
+  const [sourceVideoCaptionId, setSourceVideoCaptionId] = useState(null) // ビデオ字幕の翻訳
+  const [videoCaptionInfo, setVideoCaptionInfo] = useState("")
+
+
+  type GoogleTranslateReq = {
+    text: string, // 翻訳対象のテキスト
+    source: string, // 翻訳前の言語 （例:en）
+    target: string, // 翻訳後の言語 （例:ja）
+  }
+
+  type ExelApiTranslateReq = {
+    word: string,
+  }
+
+  // type VideoCaptionInfo = {
+  //   "Lemma": string,
+  //   "Rank": string | number,
+  //   "Frequency": string | number,
+  //   "Japanese": string,
+  //   "Commentary": string,
+  //   "cf": string | number | null,
+  //   "Ctgory": string | number | null
+  // } | null
 
   useEffect(() => {
+    console.log("デバッガー発火")
     const fetchYoutubeSubtitles = async () => { 
       const response = await fetch('http://localhost:9000/2015-03-31/functions/function/invocations', {
         method: 'POST',
@@ -85,21 +113,71 @@ function App(): JSX.Element {
     }
   }
 
-  const pressVideoCaption = (captionId: any, caption: any) => {
-    if(!translateWordId || translateWordId != captionId){
-      setTranslateWord(caption)
-      setTranslateWordId(captionId)
-    } else if(captionId == translateWordId) {
-      setTranslateWord(null)
-      setTranslateWordId(null)
+  const hiddenTranslate = () => {
+    setSourceVideoCaption(null)
+    setSourceVideoCaptionId(null)
+  }
+
+  // const translateText: any = (text: string) => {
+  //   const matchedData: any = translateJsonData.find((item: any) =>{
+  //     item.Lemma == text
+  //   })
+  //   console.log
+  //   return matchedData || null;
+  // }
+
+  const pressVideoCaption = async (captionId: any, caption: string) => {
+    if (captionId == sourceVideoCaptionId) {
+      hiddenTranslate();
+    } else {
+      setSourceVideoCaption("caption")
+      setSourceVideoCaptionId(captionId)
+
+      const url = "https://mt-auto-minhon-mlt.ucri.jgn-x.jp"
+      const key = "19c61daea69c34114464b373d74f2928064e94fd6"
+      const secret = "62c50964289e1e289de36c243098641d"
+      const name = "kyamada2001"
+      const api_name = "mt"
+      const api_param = "generalNT_en_ja"
+
+
+      // const getClientCredentials =  oauth.clientCredentials(
+      //   axios.create(),
+      //   url + '/oauth2/token.php',
+      //   key,
+      //   secret,
+      // );
+
+      // const auth: any = await getClientCredentials
+
+      await axios.post(`${url}/api/?access_token=null&key=${key}&api_name=${api_name}&api_param=${api_param}&name=${name}&type=json&text=pretty`)
+      .then((res: any) => {
+        setVideoCaptionInfo(res.data.resultset.result.text)
+      }).catch((err) => {
+        const stringJson: any = JSON.stringify(err)
+        setVideoCaptionInfo(stringJson)
+      })
     }
   }
+
 
   function pressVideoFrame() {
     setPressedVideo(true)
     setTimeout(() => {
       setPressedVideo(false)
     }, 4000);
+  }
+  
+
+  const VideoCaptionInfo = () => {
+    return (
+      <View style={styles.translateVideoCaption}>
+        <View>
+        <View><Text>翻訳表示</Text></View>
+        <Text style={styles.overlayText}>{videoCaptionInfo}</Text>
+        </View>
+      </View>
+    )
   }
 
   return (
@@ -116,21 +194,13 @@ function App(): JSX.Element {
             // onChangeState={}
           />
         </Pressable>
-        {/* 字幕をクリックした際、(pressVideoCaptionクリック時)、WebViewで翻訳を表示する */}
-        {/* {translateWordId && translateWord ?  */}
-        {/* : null} */}
+        {/* 字幕をクリックした際、(pressVideoCaption発火時)、WebViewで翻訳を表示する */}
+        {
+          videoCaptionInfo ?
+          <VideoCaptionInfo/>
+          : null
+        }
 
-        <View style={styles.translateVideoCaption}>
-           <TouchableOpacity>
-            <Text>WebView表示</Text>
-             <WebView 
-                style={styles.videoCaptionTranslateView} 
-                source={{uri:'https://dictionary.cambridge.org/ja/dictionary/english-japanese/study'}}
-                scalesPageToFit={true}
-                javaScriptEnabled={true}
-              />
-          </TouchableOpacity> 
-        </View>
         <View style={[styles.overlay, pressedVideo ? styles.overlayPress : styles.overlayNotPress]}>
           {
             currCaptions ?
@@ -193,6 +263,7 @@ const styles = StyleSheet.create({
     borderBottomColor: 'black',
   },
   captionText: {
+    zIndex: 10,
     flex:1,
     fontSize: 18,
     marginLeft: 5,
@@ -200,7 +271,7 @@ const styles = StyleSheet.create({
   translateVideoCaption: {
     flex: 1,
     // width: '85%',
-    height: 200,
+    height: '50%',
     top: '3%',
     // bottom: '40%',
     position: 'absolute',
@@ -212,7 +283,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     opacity: 0.8,
     borderBottomColor: 'black',
-    transform: [{ translateX: -50 }, { translateY: -50 }],
+    // transform: [{ translateX: -50 }, { translateY: -50 }],
   },
   videoCaptionTranslateView: {
     flex: 1,
